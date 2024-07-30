7fca1be6fdcbf49ab96578a43d30b7af08026308
David Truyens <david@toolsquare.io>
2024-07-30 16:55:35 +0200

    feat: Add NeoPixel library dependency and set monitor speed

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,31 @@
 #include <Arduino.h>
+#include "Adafruit_NeoPixel.h"
 
-void setup() {
+#define PowerPIN 11
+#define NeoPIN 12
+
+Adafruit_NeoPixel neoled = Adafruit_NeoPixel(1, NeoPIN, NEO_GRB + NEO_KHZ800);
+void setup()
+{
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(PowerPIN, OUTPUT);
+  digitalWrite(PowerPIN, HIGH);
+  neoled.begin();
+  neoled.setPixelColor(0, neoled.Color(255, 0, 0));
+  neoled.show();
+  delay(1000);
+  neoled.setPixelColor(0, neoled.Color(0, 255, 0));
+  neoled.show();
+  delay(1000);
+  neoled.setPixelColor(0, neoled.Color(0, 0, 255));
+  neoled.show();
+  delay(1000);
+  neoled.setPixelColor(0, neoled.Color(250, 0, 255));
+  neoled.show();
+  delay(1000);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
