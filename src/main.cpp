f5188958842e3ad7404a85b375e1fa559bbfb59e
GitButler <gitbutler@gitbutler.com>
2024-07-31 10:53:59 +0200

    GitButler WIP Commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,27 @@
 #include <Arduino.h>
 #include "Adafruit_NeoPixel.h"
 #include "Adafruit_AS726x.h"
+#include "logging.h"
+#include "SPI.h"
+
+uLog theLog;
 
 #define PowerPIN 11
 #define NeoPIN 12
 
 Adafruit_NeoPixel neoled = Adafruit_NeoPixel(1, NeoPIN, NEO_GRB + NEO_KHZ800);
+
+bool outputToSerial(const char* aText);
+
 void setup()
 {
+  Serial.begin(115200);
+  theLog.setOutput(0U, outputToSerial);
+  theLog.setLoggingLevel(0U, loggingLevel::Debug);
+  theLog.setColoredOutput(0U, true);
+  theLog.setIncludeTimestamp(0U, true);
+  theLog.log(subSystem::mainController, loggingLevel::Debug, "Starting up");
+
   // put your setup code here, to run once:
   pinMode(PowerPIN, OUTPUT);
   digitalWrite(PowerPIN, HIGH);
