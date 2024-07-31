666afbdd7559a7b1778eefaea9640964b3f3ca8c
David Truyens <david@toolsquare.io>
2024-07-31 11:04:12 +0200

    Adding Pascal's logging library

--- src/main.cpp
+++ src/main.cpp
@@ -23,9 +23,19 @@ void setup()
   neoled.setPixelColor(0, neoled.Color(250, 0, 255));
   neoled.show();
   delay(1000);
+
+  theLog.output(subSystem::mainController, loggingLevel::Debug, "Setup done");
+  theLog.output(subSystem::mainController, loggingLevel::Error, "Example of an error");
+  theLog.output(subSystem::mainController, loggingLevel::Warning, "Example of a warning");
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
+}
+
+bool outputToSerial(const char* aText)
+{
+  Serial.print(aText);
+  return true;
 }
